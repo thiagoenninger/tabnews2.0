44f8283377cab1db0176feedaffcf2268f7e06bc
Thiago Enninger <thiagoenninger@gmail.com>
2025-06-18 07:38:55 -0300

    Aprendendo sobre testes

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,1 +1,16 @@
-test("nome do teste", callbackFunction);
+const calculadora = require("../models/calculadora.js");
+
+test("somar 2 + 2 = 4", () => {
+  const resultado = calculadora.somar(2, 2);
+  expect(resultado).toBe(4);
+});
+
+test("somar 5 + 100 = 105", () => {
+  const resultado = calculadora.somar(5, 100);
+  expect(resultado).toBe(105);
+});
+
+test("somar 'banana' + 100 = 'Erro'", () => {
+  const resultado = calculadora.somar("banana", 100);
+  expect(resultado).toBe("Erro");
+});
